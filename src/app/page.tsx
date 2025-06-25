4919d261863ea5024251298b257241b1ceb2b2d4
Rui <s1F102501866@iniad.org>
2025-06-25 23:04:50 +0900

    '迷路をランダムで生成できるようにした　リセットボタンを付けた'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,30 @@ import styles from './page.module.css';
 // やるべきこと
 // ランダムに迷路を生成;
 // 左手の法則に従って進むキャラクター作成;
+const startBoard = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
 export default function Home() {
-  const [board, setBoard] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
+  const [board, setBoard] = useState(() => structuredClone(startBoard));
   const newBoard = structuredClone(board);
 
   const clickButton = (newBoard: number[][]) => {
-    for (let wallY = 0; wallY < 10; wallY++)
-      for (let wallX = 0; wallX < 10; wallX++)
-        if (newBoard[wallY][wallX] === 1) {
+    for (let wallY = 0; wallY < 9; wallY++)
+      for (let wallX = 0; wallX < 9; wallX++)
+        if (newBoard[wallY][wallX] === 1 && wallY % 2 === 1 && wallX % 2 === 1) {
           const randomWall = Math.floor(Math.random() * 4) + 1;
           console.log(Math.floor(Math.random() * 4) + 1);
           //Math.random() * ( 最大値 - 最小値 ) + 最小値;
-          //この場合、0から４までをランダムに生成する
+          //この場合、１から４（整数）までをランダムに生成する
           if (randomWall === 1 && newBoard[wallY - 1][wallX] === 0) {
             newBoard[wallY - 1][wallX] = 1;
           } else if (randomWall === 2 && newBoard[wallY][wallX + 1] === 0) {
@@ -38,8 +39,13 @@ export default function Home() {
           } else {
             newBoard[wallY][wallX - 1] = 1;
           }
-          setBoard(newBoard);
+          newBoard[wallY][wallX] = 1;
         }
+    setBoard(newBoard);
+  };
+
+  const resetBoard = () => {
+    setBoard(startBoard);
   };
 
   return (
@@ -55,6 +61,9 @@ export default function Home() {
       </div>
       <button className={styles.button} onClick={() => clickButton(newBoard)}>
         ポチ
+      </button>
+      <button className={styles.button} onClick={resetBoard}>
+        リセット
       </button>
     </div>
   );
